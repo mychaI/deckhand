aefe46273a94686e84f2459aaeb08bf702f74ad8
volta tumbleweed <you@example.com>
2020-01-29 20:22:12 -0800

    implement search/keyword route

--- routes/api.js
+++ routes/api.js
@@ -2,26 +2,64 @@ const router = require('express').Router();
 const axios = require('axios');
 const { client_id, client_secret } = require('../config/keys');
 
-router.post('/token/', (req, res) => {
-  const Authentication = {
-	username: client_id,
-	password: client_secret,
-  };
+let token;
+let region = 'us';
+const base_url = `https://${region}.api.blizzard.com/hearthstone/cards?locale=en_US`;
 
-  axios({
-	url: 'https://us.battle.net/oauth/token', 
-	method: 'POST', 
-	data: 'grant_type=client_credentials',
-	auth: Authentication,
-	headers: {
-	  Accept: 'application/json',
-	  'Content-Type': 'application/x-www-form-urlencoded'
-	}})
-  	.then( response => {
-	  console.log(response);
-	  res.json(response.data);
-	})
-	.catch( err=> console.log('Error: ', err));
+router.post('/token/', (req, res) => {
+  if (!token) {
+	const Authentication = {
+	  username: client_id,
+	  password: client_secret,
+	};
+
+	axios({
+	  url: 'https://us.battle.net/oauth/token', 
+	  method: 'POST', 
+	  data: 'grant_type=client_credentials',
+	  auth: Authentication,
+	  headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/x-www-form-urlencoded'
+	  }})
+	  .then( response => {
+		token = response.data;
+		console.log(token);
+		res.json({
+		  confirmation: 'token received'
+		});
+	  })
+	  .catch( err=> console.log('Error: ', err));
+  } else {
+	res.json({ 
+	  confirmation: 'ready to accept requests'
+	});
+  }
+
+});
+
+
+router.post('/search/keyword', (req,res) => {
+  const keyword = req.body.keyword;
+  if (!token) {
+	axios.post('http://localhost:5000/api/token')
+	  .then( response => { console.log('token saved') })
+	  .then( () => {
+			let request = base_url+'&keyword='+keyword+'&access_token='+token.access_token;
+			console.log(request);
+			axios.get(request)
+			  .then( data => res.json(data.data))
+			  .catch( err => console.log('Error: ', err));
+	  })
+	  .catch( err => console.log('Error: ', err));
+  } else {
+	console.log('firing');
+	let request = base_url+'&keyword='+keyword+'&access_token='+token.access_token;
+	console.log(request);
+	axios.get(request)
+	  .then( data => res.json(data.data))
+	  .catch( err => console.log('Error: ', err));
+}
 
 });
 
