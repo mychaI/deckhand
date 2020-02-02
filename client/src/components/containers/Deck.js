35e607cec5ccb5264806685a05edbe41fb23b305
volta tumbleweed <mychal.es@protonmail.com>
2020-02-02 15:45:23 -0800

    add remove icon and card name to card bar in Deck

--- client/src/components/containers/Deck.js
+++ client/src/components/containers/Deck.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const Deck = props => {
 
   const cards = props.deck.map( (card, i) => (
     <li key={i} data-id={card.id} className='card-bar'>
 	  <span>
+	    <h3 className='cardName'>{card.name}</h3>
 		<img src={card.cropImage} alt={card.name} />
+		<HighlightOffIcon className='remove'/>
 	  </span>
 	</li>
   ));
