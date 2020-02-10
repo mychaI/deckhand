9a8350e25f854dee625649e98a215c52a6803ff7
volta tumbleweed <mychal.es@protonmail.com>
2020-02-09 23:56:05 -0800

    start setting up client side auth views

--- client/src/components/containers/Login.js
+++ client/src/components/containers/Login.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const Login = () => {
 
+  const [login, setLogin] = useState({});
+  const updateField = e => {
+	setLogin({[e.target.name] : e.target.value});
+  };
+  const submitHandler = () => {
+	console.log('Clicked submit');
+  };
+
   return (
     <div>
-	  <h1>Log in to Battle.net</h1>
-	  <a href='#'>Click here to authorize</a>
+	  <h1>Log in to save and view previous decks</h1>
+	  <TextField id='input-username' label='Username' variant='outlined' name='username' value={login.username} onChange={updateField} />
+	  <TextField id='input-password' label='Password' variant='outlined' name='password' value={login.password} onChange={updateField}
+	  />
+	  <Button variant='contained' color='primary' onClick={submitHandler}>Log In</Button>
 	</div>
   )
 }
