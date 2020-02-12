7745712bc6a6bcf6033314b1a55625d3eba9683f
volta tumbleweed <mychal.es@protonmail.com>
2020-02-11 22:53:31 -0800

    style Login component; add link to Dashboard in Navbar

--- client/src/components/containers/Login.js
+++ client/src/components/containers/Login.js
@@ -3,7 +3,31 @@ import axios from 'axios';
 import { TextField, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  authContainer: {
+	gridArea: 'b',
+	height: '90%',
+	width: '400px',
+	margin: 'auto',
+	borderRadius: '30px',
+	color: 'ghostwhite',
+	background:
+	`radial-gradient(black 15%, transparent 16%) 0 0,
+	radial-gradient(black 15%, transparent 16%) 8px 8px,
+	radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+	radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;`,
+	backgroundColor:' #282828',
+	backgroundSize: '16px 16px'
+  },
+  authButton: {
+	width: '90%',
+	margin: '20px'
+  }
+});
+
 const Login = () => {
+
+  const classes = useStyles();
 
   const [login, setLogin] = useState({});
   const updateField = e => {
@@ -14,12 +38,12 @@ const Login = () => {
   };
 
   return (
-    <div>
-	  <h1>Log in to save and view previous decks</h1>
+    <div className={classes.authContainer}>
+	  <h1>Log In</h1>
 	  <TextField id='input-username' label='Username' variant='outlined' name='username' value={login.username} onChange={updateField} />
 	  <TextField id='input-password' label='Password' variant='outlined' name='password' value={login.password} onChange={updateField}
 	  />
-	  <Button variant='contained' color='primary' onClick={submitHandler}>Log In</Button>
+	  <Button variant='contained' color='primary' onClick={submitHandler} className={classes.authButton}>Log In</Button>
 	</div>
   )
 }
--- client/src/components/presentation/Navbar.js
+++ client/src/components/presentation/Navbar.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography, List, ListItem, ListItemText } from '@material-ui/core';
 import { LockOpen, Edit } from '@material-ui/icons';
 
+const useStyles = makeStyles({
+  navbar: {
+	display: 'flex',
+  },
+  title: {
+	fontSize: '30px',
+	fontFamily: 'BelweBold',
+	flex: 1,
+	marginLeft: '15px'
+  },
+  titleText: {
+	textDecoration: 'none',
+	color: '#fff',
+	cursor: 'pointer'
+  },
+  links: {
+	fontSize: '20px',
+	color: '#fff',
+	textDecoration: 'none'
+  }
+});
+
+
 const Navbar = props => {
+
+  const classes = useStyles();
 
   const guestLinks = (
     <>
 	  <ListItemText inset>
 		<Typography variant='subtitle1'>
-		  <Link to='/login'>
+		  <Link to='/login' className={classes.links}>
 			Log In
 		  </Link>
 		</Typography>
@@ -17,8 +43,28 @@ const Navbar = props => {
 
 	  <ListItemText inset>
 		<Typography variant='subtitle1'>
-		  <Link to='/signup'>
+		  <Link to='/signup' className={classes.links}>
 		    Sign Up
+		  </Link>
+		</Typography>
+	  </ListItemText>
+	</>
+  );
+
+  const userLinks = (
+    <>
+	  <ListItemText inset>
+		<Typography variant='subtitle1'>
+		  <Link to='/login' className={classes.links}>
+		    Saved Deck
+		  </Link>
+		</Typography>
+	  </ListItemText>
+
+	  <ListItemText inset>
+		<Typography variant='subtitle1'>
+		  <Link to='/signup' className={classes.links}>
+		    Log Out
 		  </Link>
 		</Typography>
 	  </ListItemText>
@@ -27,10 +73,12 @@ const Navbar = props => {
 
   return (
     <>
-	  <AppBar position='static'>
+	  <AppBar position='static' className={classes.navbar}>
 		<Toolbar>
-    	  <Typography variant='h1'>
-			Deckhand
+    	  <Typography variant='h1' className={classes.title}>
+			<Link to='/' className={classes.titleText}>
+			  Deckhand
+			</Link>
 		  </Typography>
 		  
 		  <List component='nav'>
